3fb8544cd799c1c4767cbdd59f9f42a1e28c126b
Yuuta <yuutajorand@icloud.com>
2018-03-21 08:06:45 +0100

    file added
    file added

---
+++ Avalon/Assets/Scripts/CardDraw.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card{
+	private int _id;
+	private GameObject _Img;
+    public Card (int id, GameObject Img){
+        _id = id;
+        _Img = Img;
+    }
+    public int GetId() {
+        return _id;
+    }
+    public GameObject GetImg() {
+        return _Img;
+    }
+}
+
+public class CardDraw : MonoBehaviour {
+    private List<Card> draw;
+	public GameObject[] CardDefinition;
+    Card GetCard(){
+        // Get a random number between 0 and draw.size
+        int rndcard = Random.Range(0, CardDefinition.Length);
+        Debug.Log(rndcard);
+        // Get the Card from the draw
+        Card drawcard = draw.Find(x => x.GetId() == rndcard);
+        // Remove the Card from draw
+        draw.Remove(drawcard);
+        // Return the Card
+        return drawcard;
+    }
+    List<Card> initDraw(){
+        List<Card> drawtmp = new List<Card>();
+        for (int i = 0; i < CardDefinition.Length; i++){
+            drawtmp.Add(new Card(i, CardDefinition[i]));
+        }
+        return drawtmp;
+    }
+	// Use this for initialization
+	void Start () {
+        Debug.Log(CardDefinition.Length);
+        draw = initDraw();
+        GetCard().GetImg().SetActive(true);
+        Debug.Log(draw.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		
+	}
+}
---
+++ Avalon/Assets/Scripts/VoteSys.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//public class Vote {
+
+//	private GameObject _Yes;
+//	private GameObject _No;
+//	public Vote(GameObject Yes, GameObject No)
+//	{
+//		_Yes = Yes;
+//		_No = No;
+//	}
+//	public GameObject GetYes()
+//	{
+//		return _Yes;
+//	}
+//	public GameObject GetNo()
+//	{
+//		return _No;
+//	}
+
+//}
+public class Player 
+{
+      private GameObject _PlayerId;
+      private int _Nametag;
+      private bool _PlayerStatus;
+      public Player(GameObject PlayerId, int Nametag, bool PlayerStatus)
+      {
+        _PlayerId = PlayerId;
+        _Nametag = Nametag;
+        _PlayerStatus = PlayerStatus;
+      }
+      public GameObject GetPlayerId()
+      {
+        return _PlayerId;
+      }
+      public int GetNametag()
+      {
+        return _Nametag;
+      }   
+      public bool GetPlayerStatus()
+      {
+        return _PlayerStatus;
+      }   
+}
+
+public class VoteSys : MonoBehaviour {
+    public GameObject yes;
+    public GameObject no;
+    public GameObject yes_message;
+    public GameObject no_message;
+    public GameObject[] PlayerDefinition;
+    public int no_vote = 0;
+    public int yes_vote = 0;
+    private List<Player> Plist;
+    // Use this for initialization
+    void Start() {
+        yes.GetComponent<Button>().onClick.AddListener(Yesclicked);
+    
+        no.GetComponent<Button>().onClick.AddListener(Noclicked);
+    }
+    //Player GetPlayer()
+    //{
+    //    int rndplayer = Random.Range(0, PlayerDefinition.Length);
+    //    Debug.Log(rndplayer);
+    //    Card drawcard = Plist.Find(x => x.GetNametag() == rndplayer);
+    //}
+    private void Noclicked()
+    {
+        no_vote += 1;
+        Debug.Log(no_vote);
+        if (no_vote > (8 / 2))
+        {
+            no_message.SetActive(true);
+        }
+    }
+    private void Yesclicked()
+    {
+        if (yes_vote == 0){
+            yes_vote += 1;
+        }
+        Debug.Log(yes_vote);
+        if (yes_vote > (8 / 2))
+        //if (yes_vote > (CardDefinition.size / 2))
+        {
+            yes_message.SetActive(true);
+        }
+    }
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+
+   
+
+}
